782a015467bf76f70c6d59e2780040f82beacd21
Gustavo Henrique Schmitz <gustavohenriqueschmitz568@gmail.com>
2022-07-22 09:58:30 -0300

    Adding the project

---
+++ index.js
@@ -0,0 +1,60 @@
+const calculator = require('./packages/calculator.js')
+const readline = require('readline-sync');
+
+console.log('-----------------------------------------------------')
+console.log('Initing calculator...')
+console.log('Calculator initiated!')
+
+while (true) {
+
+    let action = String
+
+    while (true) {
+        console.log('-----------------------------------------------------')
+        action = String(readline.question('Action[sum/sub/mul/div/pot/exit]: '))
+
+        if (action == 'sum' || action == 'sub' || action == 'mul' || action == 'div' || action == 'pot' || action == 'exit') {
+            break
+        }
+    }
+
+    if (action == 'exit') {
+        console.log('-----------------------------------------------------')
+        console.log('Exiting...')
+        break
+    }
+
+    console.log('-----------------------------------------------------')
+    let n1 = Number(readline.question('Num1: '))
+    let n2 = Number(readline.question('Num1: '))
+
+    if (action == 'sum') {
+        let sumResult = calculator.sum(n1,n2)
+        console.log('-----------------------------------------------------')
+        console.log('Resultado da soma: ', sumResult)
+    } 
+    
+    else if (action == 'sub') {
+        let subResult = calculator.sub(n1,n2)
+        console.log('-----------------------------------------------------')
+        console.log('Resultado da subtração: ',subResult)
+    }
+
+    else if (action == 'mul') {
+        let mulResult = calculator.mul(n1,n2)
+        console.log('-----------------------------------------------------')
+        console.log('Resultado da multiplicação: ',mulResult)
+    }
+
+    else if (action == 'div') {
+        let divResult = calculator.div(n1,n2)
+        console.log('-----------------------------------------------------')
+        console.log('Resultado da divisão: ',divResult)
+    }
+
+    else if (action == 'pot') {
+        let potResult = calculator.pot(n1,n2)
+        console.log('-----------------------------------------------------')
+        console.log('Resultado da potência: ',potResult)
+    }
+}
---
+++ packages/calculator.js
@@ -0,0 +1,26 @@
+
+const calculator = {
+    
+    sum: function (n1, n2) {
+        return n1+n2
+    },
+
+    sub: function (n1, n2) {
+        return n1-n2
+    },
+
+    mul: function (n1, n2) {
+        return n1*n2
+    },
+
+    div: function (n1, n2) {
+        return n1/n2
+    },
+
+    pot: function (n, ex) {
+        return n**ex
+    }
+
+}
+
+module.exports = calculator
