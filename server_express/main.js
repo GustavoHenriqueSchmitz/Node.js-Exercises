4f03954993b4a54a4e64d5d4004b8d24b0bb2e93
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-07-30 21:34:42 -0300

    Adding Midleware

--- server_express/main.js
+++ server_express/main.js
@@ -6,6 +6,16 @@ const server = {
     port: 8080
 }
 
+const userPermission = 'user'
+
+server.app.use('/restrict', (req, res, next)=>{
+    if (userPermission != 'admin') {
+        res.statusCode = 403
+        res.send('<h1>Invalid Permission</h1>')
+    }
+    next()
+})
+
 // Server routes
 server.app.get('/', (req, res)=>{
     res.send('<h1>Hello, World!</h1>')
@@ -13,6 +23,14 @@ server.app.get('/', (req, res)=>{
 
 server.app.get('/about', (req, res)=>{
     res.send('<h1>About Page</h1>')
+})
+
+server.app.get('/restrict', (req, res)=>{
+    res.sendFile('/home/redy/Documentos/GitHub-Local-Rep/nodejs-program-exercices/server_express/main.html')
+})
+
+server.app.get('/restrict/test', (req, res)=>{
+    res.send('This a restrict page')
 })
 
 // Initing server
